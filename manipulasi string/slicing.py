49ecba93e51dd33bc64d1250ee4de0ff2133f5e9
nurarif151 <nurarif0151@gmail.com>
2022-08-11 13:25:08 +0700

    Pemotongan string

---
+++ manipulasi string/slicing.py
@@ -0,0 +1,8 @@
+judul = 'Pelajaran Matematika Untuk SD'
+
+print(judul[0:5]) # output: Pelaj
+print(judul[:10]) # output: Pelajaran
+print(judul[10:15]) # output: Matem
+print(judul[-1:-3]) # output:
+print(judul[:-3]) # output: Pelajaran Matematika Untuk
+print(judul[-5:]) # output: uk SD
