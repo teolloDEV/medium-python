905a899de33a49483369407876e76c6b20b07367
nurarif151 <nurarif0151@gmail.com>
2022-08-27 01:38:48 +0700

    konsep programming | pemrograman prosedural

---
+++ OOP/Konsep Pemrograman/prosedural.py
@@ -0,0 +1,7 @@
+def hitung_luas():
+    alas = float(input('Masukkan alas : '))
+    tinggi = float(input('Masukkan tinggi : '))
+
+    print('Luas Segitiga adalah : ', 0.5 * alas * tinggi)
+
+hitung_luas()
