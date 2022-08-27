c261afa793a19834a1ffe5e53cf75af14842d1bf
nurarif151 <nurarif0151@gmail.com>
2022-08-27 15:54:19 +0700

    Latihan OOP kucing putih mata merah

---
+++ OOP/Latihan OOP/kucing.py
@@ -0,0 +1,10 @@
+class Kucing:
+    warna = None
+    mata = None
+
+
+kucing1 = Kucing()
+kucing1.warna = "Putih"
+kucing1.mata  = "Merah"
+
+print("Kucing itu berwarna " ,kucing1.warna, "Dan Mempunyai mata dengan warna ", kucing1.mata)
