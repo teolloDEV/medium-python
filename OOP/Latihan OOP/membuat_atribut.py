236fcb04c5d2ba8a45289d66af075261c8a4e760
nurarif151 <nurarif0151@gmail.com>
2022-08-27 17:25:06 +0700

    Latihan OOP | Membuat atribut

---
+++ OOP/Latihan OOP/membuat_atribut.py
@@ -0,0 +1,9 @@
+class Kucing:
+    nama = None
+    warna = None
+
+kucing1 = Kucing()
+kucing1.nama = 'golang'
+kucing1.warna = 'putih'
+
+print('Saya punya kucing yang bernama ', kucing1.nama, 'dan dia mempunyai bulu berwana ', kucing1.warna)
