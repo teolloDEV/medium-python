3912cea2546ae41b6270286fa34c1a2e66983a10
nurarif151 <nurarif0151@gmail.com>
2022-09-02 20:49:31 +0700

    Latihan OOP

--- OOP/Latihan OOP/oop1.py
+++ OOP/Latihan OOP/oop1.py
@@ -1,4 +1,4 @@
-class Segitiga :
+class segitiga:
     def __init__(self, alas, tinggi):
         self.alas = alas
         self.tinggi = tinggi
@@ -6,8 +6,7 @@ class Segitiga :
     def get_luas(self):
         return 0.5 * self.alas * self.tinggi
 
-segitiga1 = Segitiga(13, 4)
-segitiga2 = Segitiga(24,55)
+segitiga1 = segitiga(20,45)
 
-print('luas segitiga1:', segitiga1.get_luas())
-print('luas segitiga2:', segitiga2.get_luas())
+
+print('Luas segitiga 1 adalah : ', segitiga1.get_luas())
