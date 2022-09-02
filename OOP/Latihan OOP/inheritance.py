76c0c846a668099d604ef85188d03c69e6f778e7
nurarif151 <nurarif0151@gmail.com>
2022-09-02 20:49:15 +0700

    Latihan OOP | Belajar Inheritance

---
+++ OOP/Latihan OOP/inheritance.py
@@ -0,0 +1,18 @@
+class orang:
+    def __init__(self, nama, asal):
+        self.nama = nama
+        self.asal = asal
+
+    def perkenalan(self):
+        print(f'Perkenalkan nama saya {self.nama} dari {self.asal}')
+
+
+class pelajar:
+    pass
+
+class pekerja:
+    pass
+
+
+alex = orang('alex', 'jepara')
+alex.perkenalan()
