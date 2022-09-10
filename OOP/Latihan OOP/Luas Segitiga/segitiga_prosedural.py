2b6c07652ad37d0a440c334c5a8a06a731ab95e0
nurarif151 <nurarif0151@gmail.com>
2022-09-11 01:56:41 +0700

    Program Prosedural

---
+++ OOP/Latihan OOP/Luas Segitiga/segitiga_prosedural.py
@@ -0,0 +1,6 @@
+def hitung_luas():
+    alas = float(input('Masukkan nilai alas : '))
+    tinggi = float(input('Masukkan nilai tinggi : '))
+    print('Luas = ', 0.5 * alas * tinggi)
+
+hitung_luas()
