86b43dc7fb483c027831d9880f966ce256333a1c
nurarif151 <nurarif0151@gmail.com>
2022-09-29 20:31:17 +0700

    commit terakhir

--- OOP/Latihan OOP/membuat_atribut.py
+++ OOP/Latihan OOP/membuat_atribut.py
@@ -6,4 +6,4 @@ kucing1 = Kucing()
 kucing1.nama = 'golang'
 kucing1.warna = 'putih'
 
-print('Saya punya kucing yang bernama ', kucing1.nama, 'dan dia mempunyai bulu berwana ', kucing1.warna)
+print('Saya punya kadal yang bernama ', kucing1.nama, 'dan dia mempunyai bulu berwana ', kucing1.warna)
