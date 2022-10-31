c51f69efab90fa0bf28df91e7970637834bf5fb9
nurarif151 <nurarif0151@gmail.com>
2022-11-01 06:28:06 +0700

    Pengelompokkan tugas yang bisa digunakan berkali - kali

---
+++ Paradigma/procedural.py
@@ -0,0 +1,11 @@
+# Pengelompokkan tugas yang bisa digunakan berkali - kali
+
+def hitung_luas ():
+    alas = float (input('Masukkan alas : '))
+    tinggi = float (input('Masukkan tinggi : '))
+    print('Luas = ', 0.5 * alas * tinggi)
+
+
+# hitung luas ini bisa di panggil berkali - kali
+
+hitung_luas()
