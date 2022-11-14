f28d5fb96ee95bd7a872f946b56cede2fb8226e8
nurarif151 <nurarif0151@gmail.com>
2022-11-14 18:48:29 +0700

    Latihan OOP | Ovveriding |

--- OOP/Latihan OOP/overriding.py
+++ OOP/Latihan OOP/overriding.py
@@ -1,16 +1,61 @@
-# Penimpaan
+# class Kendaraan:
+#     def berjalan(self):
+#         print('berjalan >>>')
+# class Mobil(Kendaraan):
+#     pass
+#
+# honda = Kendaraan()
+# BMW = Mobil()
+#
+# honda.berjalan()
+# BMW.berjalan()
 
-class Kendaraan :
+# Proses overriding
+#
+# class Kendaraan:
+#     def berjalan(self):
+#         print('berjalan...')
+#
+# class Mobil(Kendaraan):
+#     def berjalan(self):
+#         print('Mobil melaju dengan kencang')
+#
+#
+# motor = Kendaraan()
+# bmw = Mobil()
+# motor.berjalan()
+# bmw.berjalan()
+
+# Menambah parameter pada fungsi yg di timpa
+# Mobil akan ditambah dengan kecepatan dan satuan kecepatan
+
+# class Kendaraan:
+#     def berjalan(self):
+#         print('berjalan...')
+#
+# class Mobil(Kendaraan):
+#     def berjalan(self, kecepatan, satuan =  'km/jam'):
+#         print(f'Mobil melaju dengan kecepatan {kecepatan} {satuan}')
+#
+#
+# motor = Kendaraan()
+# bmw = Mobil()
+# motor.berjalan()
+# bmw.berjalan(200)
+
+# memanggil fungsi pada kelas induk
+
+class Kendaraan:
     def berjalan(self):
-        print('mobil sedang berjalan...')
+        print('berjalan...')
 
 class Mobil(Kendaraan):
-    def berjalan(self):
-        print('Berjalan dengan cepat..')
-
-kendaraan1 = Kendaraan()
-mobil1 = Mobil()
+    def berjalan(self, kecepatan, satuan =  'km/jam'):
+        super().berjalan()
+        print(f'--->>> Mobil melaju dengan kecepatan {kecepatan} {satuan}')
 
 
-kendaraan1.berjalan()
-mobil1.berjalan()
+motor = Kendaraan()
+bmw = Mobil()
+motor.berjalan()
+bmw.berjalan(200)
