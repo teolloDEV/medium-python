c77d39984d22c4f0bfc20f019dde95c2ca9cd5d0
nurarif151 <nurarif0151@gmail.com>
2022-11-14 19:12:42 +0700

    Latihan OOP | GETTER SETTER |

---
+++ OOP/Latihan OOP/getter & setter/main.py
@@ -0,0 +1,20 @@
+class Mobil:
+  def __init__(self, tahun):
+    self.tahun = tahun
+
+  @property
+  def tahun(self):
+    return self.__tahun
+
+  @tahun.setter
+  def tahun(self, tahun):
+    if tahun > 2021:
+      self.__tahun = 2021
+    elif tahun < 1990:
+      self.__tahun = 1990
+    else:
+      self.__tahun = tahun
+
+
+sedan = Mobil(2200)
+print(f'Mobil ini dibuat tahun {sedan.tahun}')
